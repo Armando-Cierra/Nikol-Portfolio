6c648748296e0107a69ab3e05afdc1c37e7fa8d5
Armando Cierra Morán <armando_cierra@outlook.com>
2020-11-02 23:14:40 -0600

    Bots de google añadidos

--- pages/proyectos/index.jsx
+++ pages/proyectos/index.jsx
@@ -10,7 +10,7 @@ export default function Proyectos(){
         <>
             <Head>
                 <title>Portafolio Nikol - Proyectos</title>
-                <meta name="description" content="Diseñadora de interiores especializado en diseño residencial, comercial." />
+                <meta name="description" content="Mis últimos trabajos como diseñadora de interiores." />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossOrigin="anonymous" />
             </Head>
         
